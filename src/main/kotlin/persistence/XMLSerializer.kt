9f9b9fcb4b3cc037ea3d5ca3428f883a24f5dda8
Aidan Weeks <aidanweeks1066@gmail.com>
2024-12-03 15:10:10 +0000

    Added XML load and save to app

--- src/main/kotlin/persistence/XMLSerializer.kt
+++ src/main/kotlin/persistence/XMLSerializer.kt
@@ -1,9 +1,11 @@
-
 package persistence
 
 import com.thoughtworks.xstream.XStream
 import com.thoughtworks.xstream.io.xml.DomDriver
-import models.Note
+import ie.setu.models.Coach
+import ie.setu.models.Player
+import ie.setu.models.Team
+import ie.setu.persistence.Serializer
 import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
@@ -15,7 +17,9 @@ class XMLSerializer(private val file: File) : Serializer {
     @Throws(Exception::class)
     override fun read(): Any {
         val xStream = XStream(DomDriver())
-        xStream.allowTypes(arrayOf(Note::class.java))
+        xStream.allowTypes(arrayOf(Coach::class.java))
+        xStream.allowTypes(arrayOf(Player::class.java))
+        xStream.allowTypes(arrayOf(Team::class.java))
         val inputStream = xStream.createObjectInputStream(FileReader(file))
         val obj = inputStream.readObject() as Any
         inputStream.close()
